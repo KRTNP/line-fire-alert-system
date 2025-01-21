98902df42163a9378803ec512b5b9b22ed3aaebf
Nattaphon Honghin <154703717+KRTNP@users.noreply.github.com>
2025-01-21 14:18:08 +0700

    Delete setup.bat

--- setup.bat
+++
@@ -1,8 +0,0 @@
-# Create database
-psql -U postgres
-CREATE DATABASE wildfire_alert;
-\c wildfire_alert
-\i init.sql
-
-# Copy .env.example to .env and fill in your credentials
-cp .env.example .env
